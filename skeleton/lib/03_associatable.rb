133bfcc94742dc57bbd87483a6634e685f40a401
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-09-02 14:05:26 -0400

    Create Associatable module with has_many and belongs_to

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -10,23 +10,28 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
+
   def initialize(name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.camelize
+    @foreign_key = options[:foreign_key] || (name.to_s + "_id").to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.singularize.camelize
+    @foreign_key = options[:foreign_key] || (self_class_name.underscore + "_id").to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
