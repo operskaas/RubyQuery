4f673be47a493371ec896d1cfb34f8fa0dbaa318
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-09-02 12:23:23 -0400

    Create SQLite3Model with ::all, ::find, ::where, and #save

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -3,10 +3,27 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map do |attr_name|
+      "#{attr_name} = ?"
+    end.join(" AND ")
+
+    data = DBConnection.execute(<<-SQL, *params.values)
+    SELECT
+      *
+    FROM
+      #{table_name}
+    WHERE
+      #{where_line}
+    SQL
+
+    data.map do |datum|
+      self.new(datum)
+    end
   end
+
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
